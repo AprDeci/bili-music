18036066652568aa83307939e9dda20e55cb9b1d
Lushichen <1263868407@qq.com>
2026-03-25 16:50:24 +0800

    feat(router): 重构路由结构并添加底部导航栏
    - 使用 StatefulShellRoute 重构路由结构
    - 添加 ScaffoldWithNavBar 组件实现底部导航功能
    - 设置初始路由为 '/home'

---
+++ lib/router/ScaffoldWithNavBar.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class ScaffoldWithNavBar extends StatelessWidget {
+  final StatefulNavigationShell navigationShell;
+
+  const ScaffoldWithNavBar({super.key, required this.navigationShell});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: navigationShell, // 这里会自动显示当前 branch 的页面
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: navigationShell.currentIndex,
+        onTap: (index) {
+          navigationShell.goBranch(
+            index,
+            initialLocation: index == navigationShell.currentIndex,
+          );
+        },
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: '首页'),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: '搜索'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: '我的'),
+        ],
+      ),
+    );
+  }
+}
--- lib/router/routers.dart
+++ lib/router/routers.dart
@@ -1,11 +1,25 @@
 import 'package:bilimusic/feature/auth/ui/auth_page.dart';
+import 'package:bilimusic/router/ScaffoldWithNavBar.dart';
 import 'package:go_router/go_router.dart';
 
-final router = GoRouter(
+final GoRouter router = GoRouter(
+  initialLocation: '/home',
   routes: [
-    GoRoute(
-      path: '/',
-      builder: (context, state) => const AuthPage(),
+    StatefulShellRoute.indexedStack(
+      builder: (context, state, navigationShell) {
+        return ScaffoldWithNavBar(navigationShell: navigationShell);
+      },
+      branches: [
+        // Branch 0: Home
+        StatefulShellBranch(
+          routes: [
+            GoRoute(
+              path: '/home',
+              builder: (context, state) => const AuthPage(),
+            ),
+          ],
+        ),
+      ],
     ),
   ],
 );
