4a6e15e752ba7057452cb6f08c8c9768c707ae38
Lushichen <1263868407@qq.com>
2026-03-26 09:03:50 +0800

    feat(router): 添加底部导航栏并重构路由结构
    - 引入flutter_floating_bottom_bar包实现浮动导航栏
    - 重构路由配置为动态生成方式
    - 新增首页和个人中心页面
    - 更新导航栏样式和交互逻辑

--- lib/router/ScaffoldWithNavBar.dart
+++ lib/router/ScaffoldWithNavBar.dart
@@ -1,28 +1,71 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_floating_bottom_bar/flutter_floating_bottom_bar.dart';
 import 'package:go_router/go_router.dart';
 
-class ScaffoldWithNavBar extends StatelessWidget {
+class ScaffoldWithNavBar extends StatefulWidget {
   final StatefulNavigationShell navigationShell;
 
   const ScaffoldWithNavBar({super.key, required this.navigationShell});
 
   @override
+  State<ScaffoldWithNavBar> createState() => _ScaffoldWithNavBarState();
+}
+
+class _ScaffoldWithNavBarState extends State<ScaffoldWithNavBar> {
+  int _currentIndex = 0;
+
+  @override
+  void didUpdateWidget(covariant ScaffoldWithNavBar oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    _currentIndex = widget.navigationShell.currentIndex;
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+
     return Scaffold(
-      body: navigationShell, // 这里会自动显示当前 branch 的页面
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: navigationShell.currentIndex,
-        onTap: (index) {
-          navigationShell.goBranch(
-            index,
-            initialLocation: index == navigationShell.currentIndex,
-          );
-        },
-        items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: '首页'),
-          BottomNavigationBarItem(icon: Icon(Icons.search), label: '搜索'),
-          BottomNavigationBarItem(icon: Icon(Icons.person), label: '我的'),
-        ],
+      body: BottomBar(
+        fit: StackFit.expand,
+        borderRadius: BorderRadius.circular(40),
+        offset: 18,
+        barColor: Colors.transparent,
+        duration: const Duration(milliseconds: 300),
+        width: screenWidth * 0.92,
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(40), // 裁剪 NavigationBar 为圆角
+          child: NavigationBar(
+            selectedIndex: _currentIndex,
+            onDestinationSelected: (index) {
+              setState(() => _currentIndex = index);
+              widget.navigationShell.goBranch(
+                index,
+                initialLocation: index == widget.navigationShell.currentIndex,
+              );
+            },
+            labelBehavior: NavigationDestinationLabelBehavior.alwaysHide,
+            backgroundColor: Colors.white, // NavigationBar 背景颜色
+            elevation: 0,
+            height: 60,
+            indicatorShape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(30),
+            ),
+            destinations: const [
+              NavigationDestination(
+                icon: Icon(Icons.home_outlined),
+                selectedIcon: Icon(Icons.home),
+                label: '首页',
+              ),
+              NavigationDestination(
+                icon: Icon(Icons.person_outlined),
+                selectedIcon: Icon(Icons.person),
+                label: '我的',
+              ),
+            ],
+          ),
+        ),
+
+        body: (context, scrollController) => widget.navigationShell,
       ),
     );
   }
--- lib/router/routers.dart
+++ lib/router/routers.dart
@@ -1,5 +1,8 @@
 import 'package:bilimusic/feature/auth/ui/auth_page.dart';
+import 'package:bilimusic/feature/home/ui/home_page.dart';
+import 'package:bilimusic/feature/profile/ui/profile_page.dart';
 import 'package:bilimusic/router/ScaffoldWithNavBar.dart';
+import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 final GoRouter router = GoRouter(
@@ -10,16 +13,39 @@ final GoRouter router = GoRouter(
         return ScaffoldWithNavBar(navigationShell: navigationShell);
       },
       branches: [
-        // Branch 0: Home
-        StatefulShellBranch(
+        ...tabs
+            .map(
+              (tab) => StatefulShellBranch(
           routes: [
             GoRoute(
-              path: '/home',
-              builder: (context, state) => const AuthPage(),
+              path: tab['path'], builder: tab['builder'],
             ),
           ],
-        ),
+              ),
+            )
+            .toList(),
       ],
     ),
   ],
 );
+
+final List<Map<String, dynamic>> tabs = [
+  {
+    'path': '/home',
+    'builder': (context, state) => const HomePage(),
+    'icon': Icons.home,
+    'label': '首页',
+  },
+  {
+    'path': '/search',
+    'builder': (context, state) => const AuthPage(),
+    'icon': Icons.search,
+    'label': '搜索',
+  },
+  {
+    'path': '/profile',
+    'builder': (context, state) => const ProfilePage(),
+    'icon': Icons.person,
+    'label': '我的',
+  },
+];
