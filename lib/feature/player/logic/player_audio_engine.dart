c9b6c4f911613aacf7d1bb9f5a4cbc4a891468a9
Lushichen <1263868407@qq.com>
2026-03-30 12:27:40 +0800

    feat(播放器): 添加音频引擎逻辑实现
    实现播放器音频引擎核心功能，包括播放控制、状态管理和流处理

---
+++ lib/feature/player/logic/player_audio_engine.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/foundation.dart';
+import 'package:just_audio/just_audio.dart' as audio;
+
+class PlayerAudioEngine {
+  PlayerAudioEngine()
+    : _audioPlayer = audio.AudioPlayer(
+        useProxyForRequestHeaders: !_shouldDisableRequestHeadersProxy,
+      );
+
+  final audio.AudioPlayer _audioPlayer;
+
+  static bool get _shouldDisableRequestHeadersProxy {
+    if (kIsWeb) {
+      return true;
+    }
+    return switch (defaultTargetPlatform) {
+      TargetPlatform.windows => true,
+      TargetPlatform.linux => true,
+      _ => false,
+    };
+  }
+
+  bool get playing => _audioPlayer.playing;
+  Duration get position => _audioPlayer.position;
+  audio.ProcessingState get processingState => _audioPlayer.processingState;
+
+  Stream<Duration> get positionStream => _audioPlayer.positionStream;
+  Stream<Duration> get bufferedPositionStream =>
+      _audioPlayer.bufferedPositionStream;
+  Stream<Duration?> get durationStream => _audioPlayer.durationStream;
+  Stream<audio.PlayerException> get errorStream => _audioPlayer.errorStream;
+  Stream<audio.PlayerState> get playerStateStream =>
+      _audioPlayer.playerStateStream;
+
+  Future<Duration?> setSource({
+    required Uri uri,
+    Map<String, String>? headers,
+    dynamic tag,
+    Duration? initialPosition,
+  }) {
+    return _audioPlayer.setAudioSource(
+      audio.AudioSource.uri(uri, headers: headers, tag: tag),
+      initialPosition: initialPosition,
+    );
+  }
+
+  Future<void> play() {
+    return _audioPlayer.play();
+  }
+
+  Future<void> pause() {
+    return _audioPlayer.pause();
+  }
+
+  Future<void> stop() {
+    return _audioPlayer.stop();
+  }
+
+  Future<void> seek(Duration position) {
+    return _audioPlayer.seek(position);
+  }
+
+  Future<void> dispose() {
+    return _audioPlayer.dispose();
+  }
+}
