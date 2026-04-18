5d4f4ca871f83f5e146af13e3317fb954a0b7f69
AprDec <1263868407@qq.com>
2026-04-18 19:57:45 +0800

    refactor(player): 将play方法改为异步并使用unawaited
    避免潜在的未处理异常，同时明确表示我们不等待播放操作完成

--- lib/feature/player/logic/player_audio_engine.dart
+++ lib/feature/player/logic/player_audio_engine.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/foundation.dart';
 import 'package:just_audio/just_audio.dart' as audio;
 
@@ -57,8 +59,8 @@ class PlayerAudioEngine {
     );
   }
 
-  Future<void> play() {
-    return _audioPlayer.play();
+  Future<void> play() async {
+    unawaited(_audioPlayer.play());
   }
 
   Future<void> pause() {
